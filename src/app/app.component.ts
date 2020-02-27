a0a30734064b1e260dc203860c89e67594ce6c90
Johnston Tyler USGR <tyler.johnston@syngenta.com>
2020-02-27 13:01:19 -0500

    Change up for new app idea

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import set = Reflect.set;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'thecodedream';
+  lastUpdated = new Date();
+  now = this.lastUpdated;
+
+  constructor() {
+    setInterval(() => {
+      this.now = new Date();
+    }, 1000);
+  }
+
 }
